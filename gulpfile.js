5e78b98783cd151c08a1487c723d3397ecb35636
Siva Chegondi <smartsiva7@gmail.com>
2018-09-09 14:25:41 +0530

    publish config update

--- gulpfile.js
+++ gulpfile.js
@@ -9,22 +9,32 @@ gulp.task('clean', function(done) {
             if (error !== null) {
                 console.log('Error in deleting', error);
             }
+            done();            
         });
     }
-    done();
 });
 
 gulp.task('compile', function(done) {
-    spawn('./node_modules/.bin/ngc',['-p src/tsconfig.app.json'],{shell: true});
-    done();
+    spawn('./node_modules/.bin/ngc',['-p src/tsconfig.app.json'],{shell: true})
+        .on('close', function() {
+            fs.copyFileSync('build/package.json', 'compiled/package.json');
+            done();
+        });
 });
 
 gulp.task('package:dev', function(done) {
+    // push to your development/local registry
+    spawn('cd compiled && npm publish',['--registry http://127.0.0.1:4873'], {shell: true})
     done();
 });
 
 gulp.task('package', function(done) {
+    spawn('cd compiled | npm publish',{shell: true})
     done();
+});
+
+gulp.task('remove-package', function(done) {
+    spawn('npm unpublish shapize', ['--registry http://127.0.0.1:4873'], {shell: true});
 });
 
 gulp.task('default', gulp.series('clean', 'compile', 'package:dev'));
